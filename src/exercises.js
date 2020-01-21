1ba548b66d59575f30a566c4bd2323195a88b416
Alex Yu <alex@NEBULA.milkyway.net>
2020-01-20 23:09:09 -0500

    13

--- src/exercises.js
+++ src/exercises.js
@@ -173,17 +173,104 @@ function everywhere(values, x) {
 }
 
 function consecutive(numbers) {
-  // write your code here
+  if (!numbers || numbers.length < 3) {
+  return false;
+}
+for (let i = 0; i < numbers.length - 2; i++ ) {
+  let index = numbers[i];
+  let index_2 = numbers[i + 1];
+  let index_3 = numbers[i + 2];
+  if (Number.isInteger(index) == false || Number.isInteger(index_2) == false || Number.isInteger(index_3) == false ) {
+    return false;
+  }
+  if (index % 2 == 0 && index_2 % 2 == 0 && index_3 % 2 == 0) {
+    return true;
+  }
+  else if (index % 2 == 1 && index_2 % 2 == 1 && index_3 % 2 == 1) {
+    return true;
+  }
+}
+return false;
 }
 
 function balance(numbers) {
-  // write your code here
-  console.log("test");
+  let size = 0
+  let sum1 = 0
+  let sum2 = 0
+  let count = 0
+  if (!numbers || numbers.length < 2) {
+    return false;
+  }
+  for (i = 0; i < numbers.length; i++) {
+    if (!(Number.isInteger(numbers[i]))) {
+      return false;
+    }
+  }
+  for (z = 0; z < numbers.length; z++) {
+    if (numbers[z] == undefined) {
+      return true;
+    }
+  }
+  for (x = numbers.length; x > -1; x--) {
+    sum1 = 0
+    sum2 = 0
+    for(y = numbers.length - x; y > -1; y--) {
+      sum1 = sum1 + numbers[y]
+    }
+    for(a = numbers.length - 1; a > numbers.length - x; a--) {
+      sum2 = sum2 + numbers[a]
+    }
+    if (sum2 == sum1) {
+      count = 1
+      return true;
+    }
+  }
+  if (count == 0) {
+    return false;
+  }
 }
 
 function clumps(values) {
-  // write your code here
+
+  if (!values) {
+    return -1;
+  }
+  if (values.length < 2) {
+    return 0;
+  }
+
+  let last = values[0];
+  let current;
+  let clump = 0
+  let count = 0
+  let count2 = 0
+  
+  for (x = 1; x < values.length; x++) {
+    current = values[x]
+    if (last === current) {
+      clump = clump + 1
+      for (y = x; y < values.length; y++) {
+        last = current
+        current = values[y]
+        if (!(last === current)) {
+          count = 1
+          x = y
+          break;
+        }
+        else {
+          count = -1
+        }
+      }
+    }
+    if (count == -1) {
+      break;
+    }
+    last = current
+  }
+  return clump;
 }
+
+
 
 /*
  * Exports all functions for use in external grader.js file. Do not modify.
